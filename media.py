d5855792cccb67b0e52e414f3f532e545b914a93
Nic LoGalbo <Nic LoGalbo>
2017-04-17 19:57:38 -0500

    Comment indentation correction

--- media.py
+++ media.py
@@ -1,17 +1,16 @@
 import webbrowser
 #Import webbrowser module from standard python library
 
+"""Define and initate the controller for class Video. Title, and duration defined in this class."""
 class Video(): 
-	 """Define and initate the controller for class Video. Title, and duration defined in this class."""
 	def __init__ (self, title, duration): 
 		self.title = title
 		self.duration = duration
 
 
-
-class Movie(Video): 
-	"""Child class of Video. The only purpose that two classes exist in this code is to demonstarte inheritance. In this case the Movie class is 
+"""Child class of Video. The only purpose that two classes exist in this code is to demonstarte inheritance. In this case the Movie class is 
 	inheriting title and duration from Video. The show_trailer method calls "open" from the webbrowser module prompting the youtube url to open""" 
+class Movie(Video): 
 	def __init__ (self, title, duration, movie_storyline, poster_image, trailer_youtube): 
 		Video.__init__(self, title, duration)
 		self.storyline = movie_storyline
